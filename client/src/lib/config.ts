0eab2f497fc8669ea562822fc80b7fe822de6009
Your Name <you@example.com>
2025-11-28 20:52:20 +0530

    Fix render.yaml: change property from url to host

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5007';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5007';
+export const API_URL = apiUrl.startsWith('http') ? apiUrl : `https://${apiUrl}`;
